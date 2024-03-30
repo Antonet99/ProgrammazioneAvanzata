b2830de1dbb2b293ebbea5a7f8dad0d4c368420f
Parents99 <christianparente6@gmail.com>
2024-03-30 20:19:15 +0100

    validazione simulazione con passo aggiunta

--- src/middleware/validation.ts
+++ src/middleware/validation.ts
@@ -162,7 +162,7 @@ export async function validateSimulation(req: any, res: any, next: any) {
   if (typeof id_graph !== "number" || id_graph <= 0) {
     res
       .status(400)
-      .send({ error: "id_graph deve essere un numero/numero positivo." });
+      .send({ error: "id_graph deve essere un numero positivo." });
     return;
   }
 
@@ -181,12 +181,25 @@ export async function validateSimulation(req: any, res: any, next: any) {
     return;
   }
 
-  if ((typeof step !== "number" || step <= 0) && step > start - stop) {
-    res.status(400).send({ error: "step deve essere un numero positivo." });
+  if ((typeof step !== "number" || step <= 0) || step > stop - start) {
+    res.status(400).send({ error: "step deve essere un numero strettamente positivo e minore di stop - start" });
     return;
   }
 
-  if (
+  if(!route.start){
+    res.status(400).send("start vuoto/null");
+    return;
+  }
+  if(!route.goal){
+    res.status(400).send("goal vuot/null");
+    return;
+  }
+
+  if(route.start === route.goal){
+    res.status(400).send("nodo di partenza e nodo di arrivo uguali");
+    return;
+  }
+  /*if (
     (!route.start || !route.goal) &&
     typeof route.start !== "string" &&
     typeof route.goal !== "string"
@@ -195,9 +208,9 @@ export async function validateSimulation(req: any, res: any, next: any) {
       .status(400)
       .send({ error: "start e end devono essere una stringa non vuota." });
     return;
-  }
+  }*/
 
-  if (typeof edge.node1 !== "string" && typeof edge.node2 !== "string") {
+  /*if (typeof edge.node1 !== "string" && typeof edge.node2 !== "string") {
     res
       .status(400)
       .send({ error: "node1 e node2 devono essere una stringa non vuota." });
@@ -206,6 +219,20 @@ export async function validateSimulation(req: any, res: any, next: any) {
 
   if (edge.node1 == edge.node2) {
     res.status(400).send({ error: "node1 e node2 non possono essere uguali." });
+    return;
+  }*/
+  if(!edge.node1){
+    res.status(400).send("nodo1 vuoto/null");
+    return;
+  }
+
+  if(!edge.node2){
+    res.status(400).send("nodo2 vuoto/null");
+    return;
+  }
+
+  if(edge.node1 === edge.node2){
+    res.status(400).send("nodo1 e node2 uguali");
     return;
   }
 
